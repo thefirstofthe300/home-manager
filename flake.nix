3c5a1c4b1d625f82beab738239c30fcb6d040a7f
Danny Seymour <danny.seymour@gremlin.com>
2026-05-05 10:12:30 -0700

    fix: add CUDA cache to flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "Home Manager configuration for Daniel Seymour";
 
+  nixConfig = {
+    extra-substituters = [ "https://cuda-maintainers.cachix.org" ];
+    extra-trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
+
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
